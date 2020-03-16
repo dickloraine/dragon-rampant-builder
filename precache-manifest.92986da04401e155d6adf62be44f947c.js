80129a66ddb03301d9a359dc9203d3bc04c9c8ba
dickloraine <dickloraine@gmx.net>
2020-03-16 22:00:09 +0100

    Updates

---
+++ precache-manifest.92986da04401e155d6adf62be44f947c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19eee44ff7b1dcc73a12d9197feab091",
+    "url": "/dragon-rampant-builder/index.html"
+  },
+  {
+    "revision": "063a48b869305d34f8c7",
+    "url": "/dragon-rampant-builder/static/js/2.ab01d7cb.chunk.js"
+  },
+  {
+    "revision": "cb74030c757e88ed41d1616d3443d3e2",
+    "url": "/dragon-rampant-builder/static/js/2.ab01d7cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c228f41f54f1b23842d",
+    "url": "/dragon-rampant-builder/static/js/main.24a6e7cb.chunk.js"
+  },
+  {
+    "revision": "02fae0a539a3d323ace7",
+    "url": "/dragon-rampant-builder/static/js/runtime-main.48752ebd.js"
+  }
+]);
