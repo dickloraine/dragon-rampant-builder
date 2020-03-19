765a4cff1cba5fbfffa44430b70b5d98a86ff6f8
dickloraine <dickloraine@gmx.net>
2020-03-19 13:20:09 +0100

    Updates

---
+++ precache-manifest.25f7e13763efa492d3bd0bcf40f01ee2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96ba617a3fe60192983d6b0e77092df8",
+    "url": "/dragon-rampant-builder/index.html"
+  },
+  {
+    "revision": "57402db14d72f361cfbe",
+    "url": "/dragon-rampant-builder/static/js/2.a13ee759.chunk.js"
+  },
+  {
+    "revision": "cb74030c757e88ed41d1616d3443d3e2",
+    "url": "/dragon-rampant-builder/static/js/2.a13ee759.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78ce042492e03ad0c9ea",
+    "url": "/dragon-rampant-builder/static/js/main.5f04f028.chunk.js"
+  },
+  {
+    "revision": "02fae0a539a3d323ace7",
+    "url": "/dragon-rampant-builder/static/js/runtime-main.48752ebd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dragon-rampant-builder/precache-manifest.b0dbeea9a3a2e821b0b133469474357d.js"
+  "/dragon-rampant-builder/precache-manifest.25f7e13763efa492d3bd0bcf40f01ee2.js"
 );
 
 self.addEventListener('message', (event) => {
