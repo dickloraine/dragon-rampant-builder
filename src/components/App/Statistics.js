97ae0172020ae43e39a7fa10d3f87baee0796798
dickloraine <dickloraine@gmx.net>
2020-03-19 14:32:45 +0100

    hide Pir values if zero

--- src/components/App/Statistics.js
+++ src/components/App/Statistics.js
@@ -166,6 +166,7 @@ const renderCustomizedLabel = ({
   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
   const x = cx + radius * Math.cos(-midAngle * RADIAN);
   const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  const percentValue = (percent * 100).toFixed(0);
 
   return (
     <text
@@ -175,7 +176,7 @@ const renderCustomizedLabel = ({
       textAnchor={x > cx ? 'start' : 'end'}
       dominantBaseline="central"
     >
-      {`${(percent * 100).toFixed(0)}%`}
+      {percentValue > 0 ? `${percentValue}%` : ''}
     </text>
   );
 };
