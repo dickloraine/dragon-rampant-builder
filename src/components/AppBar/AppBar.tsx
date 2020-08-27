1fc4ced2d52d9ec75e70fe9e0e65ceb8c19bb3e9
dickloraine <dickloraine@gmx.net>
2020-08-27 13:39:35 +0200

    refactored toggling the view mode

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -4,51 +4,22 @@ import {
   Hidden,
   IconButton,
   Toolbar,
-  Tooltip,
   Typography,
 } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
 import ReplayIcon from '@material-ui/icons/Replay';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { newRoster } from 'store/rosterSlice';
-import { RootState } from 'store/store';
-import { UIState, updateUI } from 'store/uiSlice';
 import LoadList from '../LoadList';
 import SaveList from '../SaveList';
 import SideMenu from '../SideMenu';
+import ToggleViewMode from './ToggleViewMode';
 import TotalPoints from './TotalPoints';
 
 const AppBar = () => {
   const dispatch = useDispatch();
-  const ui = useSelector((state: RootState) => state.ui);
-
-  const changeViewMode = (clicked: keyof UIState, newState: boolean) => {
-    const notClicked = clicked === 'viewMode' ? 'editMode' : 'viewMode';
-    let newStates = { [clicked]: newState };
-    if (newState) newStates = { ...newStates, [notClicked]: false };
-    dispatch(updateUI(newStates));
-  };
-
-  const ToggleUIButton: React.FC<{
-    option: keyof UIState;
-    Icon: any;
-    title: string;
-    onClick: (arg0: keyof UIState, arg1: boolean) => void;
-  }> = ({ option, Icon, title, onClick }) => (
-    <Tooltip title={title}>
-      <IconButton
-        color="inherit"
-        aria-label={title}
-        onClick={() => {
-          onClick(option, !ui[option]);
-        }}
-      >
-        <Icon fontSize="small" color={ui[option] ? 'inherit' : 'disabled'} />
-      </IconButton>
-    </Tooltip>
-  );
 
   return (
     <Box display="flex">
@@ -76,18 +47,8 @@ const AppBar = () => {
           </Box>
           <Box flexGrow={1}></Box>
           <Box display="flex" alignItems="center">
-            <ToggleUIButton
-              option="viewMode"
-              Icon={VisibilityIcon}
-              title="View mode"
-              onClick={changeViewMode}
-            />
-            <ToggleUIButton
-              option="editMode"
-              Icon={EditIcon}
-              title="Edit mode"
-              onClick={changeViewMode}
-            />
+            <ToggleViewMode option="viewMode" Icon={VisibilityIcon} title="View mode" />
+            <ToggleViewMode option="editMode" Icon={EditIcon} title="Edit mode" />
             <TotalPoints />
           </Box>
         </Toolbar>
