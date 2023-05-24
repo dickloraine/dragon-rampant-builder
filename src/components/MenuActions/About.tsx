261b2cd37e08a62f6ba8e532271b6fbc52c05967
dickloraine <dickloraine@gmx.net>
2023-05-24 20:12:55 +0000

    consolidate menu actions

--- src/components/MenuActions/About.tsx
+++ src/components/MenuActions/About.tsx
@@ -6,37 +6,38 @@ import {
   Dialog,
   DialogContent,
   DialogTitle,
-  IconButton,
-  Tooltip,
   Typography,
 } from '@mui/material';
 import React from 'react';
-import useOpen from '../hooks/useOpen';
+import useOpen from '../../hooks/useOpen';
+import MenuAction from './MenuAction';
 
-const About: React.FC<{ onClose?: () => void }> = ({ onClose = undefined }) => {
+const About: React.FC<{ onClose?: () => void; showText?: boolean }> = ({
+  onClose = undefined,
+  showText = true,
+}) => {
   const [open, handleOpen, handleClose] = useOpen(false, undefined, onClose);
 
   return (
-    <>
-      <Tooltip title="Load List">
-        <IconButton color="inherit" onClick={handleOpen} size="large">
-          <InfoIcon />
-        </IconButton>
-      </Tooltip>
-      {<Typography onClick={handleOpen}>About</Typography>}
+    <MenuAction
+      text="About"
+      action={handleOpen}
+      icon={<InfoIcon />}
+      showText={showText}
+    >
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle sx={{ textAlign: 'center' }}>About</DialogTitle>
         <DialogContent>
           <Typography variant="body1">
             A simple Army Builder for the tabletop game{' '}
-            {<a href="https://ospreypublishing.com/dragon-rampant">Dragon Rampant</a>}.
+            {<a href="https://ospreypublishing.com/xenos-rampant">Xenos Rampant</a>}.
           </Typography>
           <Box display="flex" sx={{ mt: 3 }}>
             <Chip
               label="View on Github"
               icon={<GitHubIcon />}
               component="a"
-              href="#chip"
+              href="https://github.com/dickloraine/xenos-rampant-builder"
               clickable
             />
             <Box flexGrow={1}></Box>
@@ -44,7 +45,7 @@ const About: React.FC<{ onClose?: () => void }> = ({ onClose = undefined }) => {
           </Box>
         </DialogContent>
       </Dialog>
-    </>
+    </MenuAction>
   );
 };
 
